05622dd3e0876a005beb736e0f09b7e46ac5df87
Jose-17-11 <ariashdez@outlook.com>
2024-11-10 15:50:25 -0600

    add authentication in all pages

---
+++ scripts/verify-token.js
@@ -0,0 +1,34 @@
+async function checkToken() {
+    const token = localStorage.getItem('authToken');
+
+    // Si no hay token, redirige al usuario al login
+    if (!token) {
+        window.location.href = "Pagina1.html";
+        return;
+    }
+
+    // Verificar el token en el servidor
+    try {
+        const response = await fetch("http://localhost:3000/verificar-token", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`
+            }
+        });
+        if (!response.ok) {
+            // Si el token es inválido o ha expirado
+            throw new Error("Token inválido o expirado");
+        }
+        // Si el token es válido, el usuario puede permanecer en la página
+        console.log("Token válido");
+
+    } catch (error) {
+        console.error("Error:", error.message);
+        // Eliminar el token de localStorage
+        localStorage.removeItem('authToken');
+        // Redirigir al login
+        window.location.href = "Pagina1.html";
+    }
+}
+checkToken()
